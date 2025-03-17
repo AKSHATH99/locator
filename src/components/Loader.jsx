e17c66afb018636ded3a9c4778972b0f8e81c02c
Akshath P <akshathpkk@gmail.com>
2025-03-17 22:06:07 +0530

    loader animation

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div class="">
+    <div class="w-5 h-5 border-4 border-[#92DE46] border-dashed rounded-full animate-spin"></div>
+  </div>
+    
+  )
+}
+
+export default Loader
